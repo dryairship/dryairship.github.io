c126d67b234e1f1fbfb70082b5a9e4cfa765def8
Priydarshi Singh <dryairship@gmail.com>
2020-02-14 13:52:01 +0530

    Add d7843h83ha92

---
+++ d7843h83ha92/heartforadditi.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <math.h>
+
+#define Priydarshi /* Your valentine. XD */
+#define wants /* YOU. Just YOU. */
+#define to letAdditiPlayWithRandomLongs( /* Mathematicians love playing with long numbers */
+#define Additi ); printf("\n"); /* Eh, ignore. :p */
+#define impress 618475327488 /* I want my impression score to be 618475327488 out of 100. */
+#define hug 40210559963712 /* Yeah, I want to hug you 40210559963712 times. */
+#define flirt 2573485501354569 /* And flirt 2573485501354569 times.  */
+#define with /* with you, of course. */
+#define cuddle 40210710958656 /* And cuddle with you for 40210710958656 minutes.  */
+#define kiss 628292358144 /* And kiss you 628292358144 times. */
+#define canoodle 9817030656 /* And conoodle with you for 9817030656 hours. */
+#define love 150994944 /* And love you for 150994944 years, maybe. */
+#define go 2573516006789705 /* Is it even possible to go out on 2573516006789705 dates? */
+#define out /* going in would be okay too ;) */
+#define wish 1<<100 /* I wish that for every 1 wish of mine fulfilled, 100 wishes of yours are fulfilled. */
+#define a /* Literally, a means one. You are the one for Priydarshi. <3 */
+#define Happy /* Stay happy. */
+#define Valentines /* Be my valentine. */
+#define Day /* Have a nice day! */
+
+int letAdditiPlayWithRandomDoubles(double x){
+    return (int)(-0.9*pow(x,5)+12.458*pow(x,4)-61.25*pow(x,3)+123.54*pow(x,2)-70.85*x+32.3);
+}
+
+void letAdditiPlayWithRandomLongs(long l){
+    for(int i=0; i<18; i++, l>>=3)
+        printf("%c",letAdditiPlayWithRandomDoubles(l%8));
+}
+
+int main(){
+    Priydarshi wants to impress Additi
+    Priydarshi wants to hug Additi
+    Priydarshi wants to flirt with Additi
+    Priydarshi wants to go out with Additi
+    Priydarshi wants to cuddle with Additi
+    Priydarshi wants to kiss Additi
+    Priydarshi wants to canoodle with Additi
+    Priydarshi wants to love Additi
+    Priydarshi wants to wish Additi a Happy Valentines Day
+    //                                               ^ insert apostrophe here
+}
